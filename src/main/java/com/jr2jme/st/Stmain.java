a5e48f49ceef6f3feafdf15f67477511d026e288
mhffdq <mhffdq@gmail.com>
2014-03-28 20:49:36 +0900

    wikitext

---
+++ src/main/java/com/jr2jme/st/Stmain.java
@@ -0,0 +1,149 @@
+package com.jr2jme.st;
+
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.Mongo;
+import org.mongojack.JacksonDBCollection;
+
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.UnknownHostException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+/**
+ * Created by Hirotaka on 2014/03/21.
+ */
+public class Stmain {
+
+
+    public static void main(String[] args){
+        System.out.println(args[0]);
+        Mongo mongo=null;
+        try {
+            mongo = new Mongo("192.168.11.6",27017);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        assert mongo != null;
+        DB db=mongo.getDB("wikipedia_test");
+        DBCollection dbCollection=db.getCollection("text_test2");
+        XMLInputFactory factory = XMLInputFactory.newInstance();
+
+        XMLStreamReader reader = null;
+        BufferedInputStream stream = null;
+        JacksonDBCollection<Wikitext,String> coll = JacksonDBCollection.wrap(dbCollection, Wikitext.class,String.class);
+        try {
+            // 2. 入力に使用するファイルの設定
+            stream = new BufferedInputStream(
+                    new FileInputStream(args[0]));
+
+            // 3. パーサの生成
+            try {
+                reader = factory.createXMLStreamReader(stream);
+            } catch (XMLStreamException e) {
+                e.printStackTrace();
+            }
+
+            // 4. イベントループ
+
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy'-'MM'-'dd'T'HH':'mm':'ss'Z'");
+            sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
+            Boolean inrev=false;
+            Boolean incon=false;
+            String comment="";
+            String title= "";
+            String name="";
+            Date date=null;
+            String text;
+            int id=0;
+            Boolean changetitleflag=false;
+            int countarticle=0;
+            assert reader != null;
+            while (reader.hasNext()) {
+                // 4.1 次のイベントを取得
+                int eventType = reader.next();
+                // 4.2 イベントが要素の開始であれば、名前を出力する
+                if (eventType == XMLStreamReader.START_ELEMENT) {
+                    if("title".equals(reader.getName().getLocalPart())){
+                        //System.out.println(reader.getElementText());
+                        title = reader.getElementText();
+                        if(changetitleflag){
+                            countarticle++;
+                            if(countarticle>10) {
+                                break;
+                            }
+                        }
+                    }
+                    if("revision".equals(reader.getName().getLocalPart())){
+                        inrev=true;
+                    }
+                    if("id".equals(reader.getName().getLocalPart())){
+                        if(inrev==true&&incon==false){
+                            id=Integer.valueOf(reader.getElementText());
+                        }
+                    }
+                    if("comment".equals(reader.getName().getLocalPart())) {
+                        comment=reader.getElementText();
+
+                    }
+                    if("timestamp".equals(reader.getName().getLocalPart())){
+                        //System.out.println(reader.getElementText());
+                        try {
+                            date = sdf.parse(reader.getElementText());
+                        } catch (ParseException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    if("ip".equals(reader.getName().getLocalPart())){
+                        //System.out.println(reader.getElementText());
+                        name=reader.getElementText();
+                        incon=true;
+                    }
+                    if("username".equals(reader.getName().getLocalPart())){
+                        //System.out.println(reader.getElementText());
+                        name=reader.getElementText();
+                        incon=true;
+                    }
+                    if("text".equals(reader.getName().getLocalPart())){
+                        //System.out.println(reader.getElementText());
+                        text = reader.getElementText();
+                        coll.insert(new Wikitext(title,date,name,text,id,comment));
+                        changetitleflag=true;
+                        inrev=false;
+                        incon=false;
+                    }
+                    //System.out.println(reader.getName().getLocalPart());
+                }
+            }
+        } catch (FileNotFoundException ex) {
+            System.err.println(args[0] + " が見つかりません");
+        } catch (XMLStreamException ex) {
+            System.err.println(args[0] + " の読み込みに失敗しました");
+        } finally {
+            // 5. パーサ、ストリームのクローズ
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (XMLStreamException ex) {
+                    ex.printStackTrace();
+                }
+            }
+            if (stream != null) {
+                try {
+                    stream.close();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/com/jr2jme/st/Wikitext.java
@@ -0,0 +1,87 @@
+package com.jr2jme.st;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.mongojack.ObjectId;
+
+import java.util.Date;
+
+/**
+ * Created by Hirotaka on 2014/03/23.
+ */
+public class Wikitext {
+    String title;
+    Date date;
+    String name;
+    String text;
+    String comment;
+    int revid;
+    public Wikitext(){//forjackson dummy
+
+    }
+    public Wikitext(String titlea, Date datea, String namea, String texta, int id, String com){
+        title=titlea;
+        date=datea;
+        name=namea;
+        text=texta;
+        revid=id;
+        comment=com;
+    }
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getTitle(){
+        return title;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public void setRevid(int revid) {
+        this.revid = revid;
+    }
+
+    public int getRevid() {
+        return revid;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    private String id;
+    @ObjectId
+    @JsonProperty("_id")
+    public String getId() {
+        return id;
+    }
+    @ObjectId
+    @JsonProperty("_id")
+    public void setId(String id) {
+        this.id = id;
+    }
+}
